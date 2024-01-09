9fb51f74145f142c52e2ad57d9b4f242d580623f
neuropathy <96810159+neuropathy0829@users.noreply.github.com>
2024-01-09 23:26:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,28 @@ function hideElements() {
     element.style.display = 'none';
   });
 }
+//禁用右鍵
+document.addEventListener('contextmenu', function (event) {
+  event.preventDefault();
+}, true);
+
+//禁用F12、Ctrl+Shift+I、Ctrl+Shift+J、Ctrl+U
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    return false;
+  }
+}
+
+// 使用JavaScript禁止整個頁面的文本選取
+document.addEventListener('selectstart', (e) => {
+  e.preventDefault();
+});
